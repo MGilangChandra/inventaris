426019f97ca5c8fdf5e46e2941e8438672e1fa60
MGilangChandra <gilangchan2107@gmail.com>
2024-09-12 15:41:15 +0700

    Update : Beranda

--- Modules/Auth/resources/views/pegawai-login.blade.php
+++ Modules/Auth/resources/views/pegawai-login.blade.php
@@ -1,9 +1,9 @@
 @extends('auth::layouts.master')
 
 @section('content')
-    <aside class="bg-white w-full lg:w-[624px] fixed top-0 left-0 h-screen pt-36">
+    <aside class="bg-white px-4 py-6 w-full lg:w-[624px] fixed top-0 left-0 h-screen pt-36">
         <h1 class="font-bold text-4xl text-center">Login</h1>
-        <form action="" class="p-4 py-6 gap-4 md:gap-8 flex flex-col">
+        <form action="" class=" gap-4 md:gap-8 flex flex-col">
             <label>
                 <p class="font-semibold">Username</p>
                 <input type="text" name="username" required class="border-[3px] border-[#4f4f4f] rounded-2xl flex w-full p-2 outline-none bg-transparent">
@@ -14,5 +14,7 @@
             </label>
             <button type="submit" class="bg-[#4f4f4f] rounded-2xl flex w-full p-3 outline-none justify-center text-[#FFF7FC] font-semibold mt-8">Enter</button>
         </form>
+        <a href="{{ route('home') }}" class="border-2 border-[#0008] rounded-2xl flex w-full p-3 outline-none justify-center text-black  font-semibold mt-4">Kembali</a>
     </aside>
+    <a href="{{  route('login') }}" class="absolute top-5 left-2 flex items-center font-bold text-black"><box-icon name='chevron-left'></box-icon>Admin</a>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -15,10 +15,10 @@
     <nav class="flex items-center justify-between backdrop-blur-sm h-16 w-full md:px-[10%] px-[5%] ">
         <h1 class="text-2xl font-bold ">Inventaris</h1>
         <ul class="hidden gap-8 font-semibold items-center md:flex ">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Kontak</a></li>
-            <li><a class="bg-[#C07F00] py-2 px-4 rounded-3xl flex items-center justify-center" href="#">Login</a>
+            <li><a href="#" class="duration-150 hover:opacity-50">Home</a></li>
+            <li><a href="#" class="duration-150 hover:opacity-50">Profile</a></li>
+            <li><a href="#" class="duration-150 hover:opacity-50">Kontak</a></li>
+            <li><a class="bg-[#C07F00] py-2 px-4 rounded-3xl flex items-center justify-center" href="{{ route('login') }}">Login</a>
             </li>
         </ul>
     </nav>
@@ -26,7 +26,7 @@
     <section class="h-[calc(100vh-64px)] py-8 md:px-[10%] px-[5%]">
         <div class="flex gap-6 items-center flex-wrap w-full">
             <p class="cursor-pointer"><box-icon name='filter-alt' class="size-10" color='#ffffff'></box-icon></p>
-            <label for="" class="flex items-center gap-2 px-2 py-2 border-2 border-[#fff5] rounded-3xl w-full">
+            <label for="" class="flex items-center gap-2 px-2 py-2 border-2 border-[#fff5] rounded-3xl w-full hover:shadow-[0px_0px_12px_-5px_#fff5] duration-150">
                 <box-icon name='search' color='#ffffff'></box-icon>
                 <input type="text" placeholder="Cari..." class="bg-transparent text-white flex outline-none w-full">
             </label>
@@ -37,14 +37,40 @@
                 HOT
                 <box-icon name="hot" class="size-10" color="#DE3636" type="solid"></box-icon>
             </h1>
-            <div class="gap-4 mt-2 scrollbar-none overflow-x-auto scroll-snap-x mandatory flex">
-                <div class="flex-grow scroll-snap-align-center flex-shrink-0 bg-red-600 h-96 min-w-72 rounded-2xl max-w-80 relative">
-                    <a href="" class="absolute top-0 right-0"><box-icon name='link-external' class="p-2" color='#ffffff' ></box-icon></a>
+            <div class="gap-4 mt-2 py-4 scrollbar-none overflow-x-scroll scroll-snap-x mandatory flex">
+                {{-- List --}}
+                <div class="flex-grow scroll-snap-align-center overflow-hidden flex-shrink-0 bg-slate-500 h-96 min-w-72 rounded-3xl max-w-80 relative">
+                    <a href="#" class="absolute top-1 right-1 p-2 rounded-full duration-150 bg-slate-700 hover:bg-opacity-80 flex items-center justify-center z-10"><box-icon name='paper-plane' color='#ffffff' ></box-icon></a>
+                    <div class="mb-1 w-full h-3/4  bg-slate-900">
+                        <img src="https://awsimages.detik.net.id/community/media/visual/2023/11/18/gunung-semeru_169.png?w=600&q=90" class="object-cover w-full h-full hover:opacity-75 duration-300" alt="">
+                    </div>
+                    <h1 class="text-center font-bold text-2xl">Nama</h1>
+                    <a href="#" class="z-10 font-bold absolute duration-300 w-14 hover:w-[calc(100%-16px)] p-1 bg-slate-700 bottom-2 right-2 h-12 rounded-3xl flex items-center justify-center">Lihat</a>
                 </div>
-                {{-- <div class="flex-grow scroll-snap-align-center flex-shrink-0 bg-red-600 h-96 min-w-72 rounded-2xl max-w-80"></div>
-                <div class="flex-grow scroll-snap-align-center flex-shrink-0 bg-red-600 h-96 min-w-72 rounded-2xl max-w-80"></div>
-                <div class="flex-grow scroll-snap-align-center flex-shrink-0 bg-red-600 h-96 min-w-72 rounded-2xl max-w-80"></div>
-                <div class="flex-grow scroll-snap-align-center flex-shrink-0 bg-red-600 h-96 min-w-72 rounded-2xl max-w-80"></div> --}}
+                <div class="flex-grow scroll-snap-align-center overflow-hidden flex-shrink-0 bg-slate-500 h-96 min-w-72 rounded-3xl max-w-80 relative">
+                    <a href="#" class="absolute top-1 right-1 p-2 rounded-full duration-150 bg-slate-700 hover:bg-opacity-80 flex items-center justify-center z-10"><box-icon name='paper-plane' color='#ffffff' ></box-icon></a>
+                    <div class="mb-1 w-full h-3/4  bg-slate-900">
+                        <img src="https://asset.kompas.com/crops/OD9itl1d8QHvxgLLaKN3u13KhYw=/1x0:1000x666/750x500/data/photo/2023/02/28/63fdb9789cf09.jpg" class="object-cover w-full h-full hover:opacity-75 duration-300" alt="">
+                    </div>
+                    <h1 class="text-center font-bold text-2xl">Nama</h1>
+                    <a href="#" class="z-10 font-bold absolute duration-300 w-14 hover:w-[calc(100%-16px)] p-1 bg-slate-700 bottom-2 right-2 h-12 rounded-3xl flex items-center justify-center">Lihat</a>
+                </div>
+                <div class="flex-grow scroll-snap-align-center overflow-hidden flex-shrink-0 bg-slate-500 h-96 min-w-72 rounded-3xl max-w-80 relative">
+                    <a href="#" class="absolute top-1 right-1 p-2 rounded-full duration-150 bg-slate-700 hover:bg-opacity-80 flex items-center justify-center z-10"><box-icon name='paper-plane' color='#ffffff' ></box-icon></a>
+                    <div class="mb-1 w-full h-3/4  bg-slate-900">
+                        <img src="https://asset-2.tstatic.net/tribunnewswiki/foto/bank/images/gunung-sinabung-dari-desa-tiga-serangkai-karo-kamis-04062015.jpg" class="object-cover w-full h-full hover:opacity-75 duration-300" alt="">
+                    </div>
+                    <h1 class="text-center font-bold text-2xl">Nama</h1>
+                    <a href="#" class="z-10 font-bold absolute duration-300 w-14 hover:w-[calc(100%-16px)] p-1 bg-slate-700 bottom-2 right-2 h-12 rounded-3xl flex items-center justify-center">Lihat</a>
+                </div>
+                <div class="flex-grow scroll-snap-align-center overflow-hidden flex-shrink-0 bg-slate-500 h-96 min-w-72 rounded-3xl max-w-80 relative">
+                    <a href="#" class="absolute top-1 right-1 p-2 rounded-full duration-150 bg-slate-700 hover:bg-opacity-80 flex items-center justify-center z-10"><box-icon name='paper-plane' color='#ffffff' ></box-icon></a>
+                    <div class="mb-1 w-full h-3/4  bg-slate-900">
+                        <img src="https://lh5.googleusercontent.com/proxy/A2RdkoCMsChOYd3bd1m0qCGX_1sAtbZtPUE71_GlHHCSKDMCstFcq5hXvAV_CdBNNt2MP_ZEGJAb-9-tpR5LwsqDGvENdYG6zE4527YNvt7xgtKxjR2L68EVetzIJfQSQ_egXYV3RMw" class="object-cover w-full h-full hover:opacity-75 duration-300" alt="">
+                    </div>
+                    <h1 class="text-center font-bold text-2xl">Nama</h1>
+                    <a href="#" class="z-10 font-bold absolute duration-300 w-14 hover:w-[calc(100%-16px)] p-1 bg-slate-700 bottom-2 right-2 h-12 rounded-3xl flex items-center justify-center">Lihat</a>
+                </div>
             </div>
         </div>
     </section>
